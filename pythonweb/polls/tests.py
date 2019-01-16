d41cbf6706a4a0924cecb6efd176864919803d72
Shekcon <quangsang9773@gmail.com>
2019-01-16 22:32:16 +0700

    - Add setup test create database for testcase - Add test to reponse each page and test to display content default in different situations

--- pythonweb/polls/tests.py
+++ pythonweb/polls/tests.py
@@ -1,34 +1,44 @@
 from django.test import TestCase
 from .models import Question
-from random import randrange
+
+
 # Create your tests here.
 class TestPolls(TestCase):
 
     index_page = '/'
 
-    def test_was_reponse_each_page(self):
+    def setUp(self):
+        Question.objects.create(question_text="What movie to see on Friday 16/1/2019")
+        Question.objects.create(question_text="What movie to see on Sunday 20/1/2019")
+        Question.objects.create(question_text="What movie to see on Monday 21/1/2019")
+
+    def test_was_reponse_home_page(self):
         reponse = self.client.get(self.index_page)
-        self.assertEqual(200, reponse.status_code)
-    
-    def test_was_show_noavaiable_question(self):
+        for que in Question.objects.all():
+            self.assertContains(reponse, que.question_text)
+            
+    def test_was_show_detail_question(self):
         for ques in Question.objects.all():
-            if not ques.answer:
-                reponse = reponse = self.client.get(self.index_page + str(ques.id) + "/")
-                self.assertContains(reponse.content, 'No available option')
-                self.assertContains(reponse.content, 'Go back')
+            reponse = self.client.get(self.index_page + str(ques.id) + "/")
+            if not ques.answer.all():
+                self.assertContains(reponse, 'No available option')
+                self.assertContains(reponse, 'Go back')
+                self.assertNotContains(reponse, 'Sumbit')
+            else:
+                self.assertContains(reponse, 'Sumbit')
+                for movie in ques.answer.all():
+                    self.assertContains(reponse, movie.choice_text)
 
     def test_was_reponsed_result(self):
         for ques in Question.objects.all():
-            reponse = self.client.get(self.index_page + str(ques.id) + "/result")
-            self.assertEqual(reponse.status_code, 200)
-            self.assertContains(reponse.content, 'Go back')
+            reponse = self.client.get(self.index_page + str(ques.id) + "/result/")
+            self.assertContains(reponse, 'Go back')
     
-    def test_show_404_page(self):
+    def test_object_not_found(self):
         for i in range(0, 100):
-            reponse = reponse = self.client.get(self.index_page + str(i))
+            reponse = self.client.get(self.index_page + str(i) + "/")
             try:
                 Question.objects.get(id=i)
-            except Question.DoesNotExist:
-                self.assertEqual(reponse.status_code, 404)
-            else:
                 self.assertEqual(reponse.status_code, 200)
+            except Question.DoesNotExist:
+                self.assertEqual(reponse.status_code, 404)      
